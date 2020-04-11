e2b33a13605f4964d36e1b217b1cdbf7f7ea9294
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-11 17:06:38 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+//#include "gtest/gtest.h"
+#include "../traveling_salesman/Source.cpp"
+
+TEST(Test_superStringCost1, superStringCost) {
+	Vstr v = { "a", "aa", "sdf", "fa", "ahg", "hju", "ut" };
+	EXPECT_EQ(11, superStringCost(v));
+	EXPECT_TRUE(true);
+}
+
+TEST(Test_superStringCost2, superStringCost) {
+	Vstr v = { "a", "aa", "aaa" };
+	EXPECT_EQ(3, superStringCost(v));
+	EXPECT_TRUE(true);
+}
+
+TEST(Test_superStringCost3, superStringCost) {
+	Vstr v = { "cde", "abc", "eab", "fgh", "ghf", "hed"};
+	EXPECT_EQ(11, superStringCost(v));
+	EXPECT_TRUE(true);
+}
+
+
+int main2(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
