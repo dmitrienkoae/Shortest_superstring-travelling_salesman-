f70a6f6512255debc28459a070ea32193b6ef457
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-14 20:45:27 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,19 +2,39 @@
 //#include "gtest/gtest.h"
 #include "../traveling_salesman/Source.cpp"
 
-TEST(Test_superStringCost1, superStringCost) {
+TEST(Test_superStringCost1, superString) {
 	Vstr v = { "a", "aa", "sdf", "fa", "ahg", "hju", "ut" };
-	EXPECT_EQ(11, superStringCost(v));
+  	vector::string super_str = superString(v);
+ 	bool b = true;
+ 	for (auto i : v){
+   	if( super_str.find(i) == string::npos)
+     		 b = false;
+	EXPECT_EQ(b, true);
+	EXPECT_EQ(11, super_str.size());
 }
 
-TEST(Test_superStringCost2, superStringCost) {
+TEST(Test_superStringCost2, superString) {
 	Vstr v = { "a", "aa", "aaa" };
 	EXPECT_EQ(3, superStringCost(v));
+	Vstr v = { "a", "aa", "aaa" };
+	vector::string super_str = superString(v);
+ 	bool b = true;
+  	for (auto i : v){
+    	if( super_str.find(i) == string::npos)
+    		b = false;
+	EXPECT_EQ(b, true);
+	EXPECT_EQ(3, super_str.size());
 }
 
-TEST(Test_superStringCost3, superStringCost) {
+TEST(Test_superStringCost3, superString) {
 	Vstr v = { "cde", "abc", "eab", "fgh", "ghf", "hed"};
-	EXPECT_EQ(11, superStringCost(v));
+	vector::string super_str = superString(v);
+  	bool b = true;
+  	for (auto i : v){
+   	if( super_str.find(i) == string::npos)
+      		b = false;
+	EXPECT_EQ(b, true);
+	EXPECT_EQ(13, super_str.size());
 }
 
 
