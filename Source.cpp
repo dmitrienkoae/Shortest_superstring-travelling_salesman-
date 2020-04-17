2c8ec2c2b49e3ac10abfcda5683c0380fa604929
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-17 23:00:47 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,10 @@
 
 int main() {
 	std::vector<std::string> vs = {"eab", "fgh", "ghf", "hed", "abc", "cde"};
-	std::cout << "Sum length : "<< super_string(vs).size() << std::endl;
-	std::cout << "Superstring : " <<super_string(vs) << std::endl;
+	std::cout << "Sum length : "<< superString(vs).size() << std::endl;
+	std::cout << "Superstring : " <<superString(vs) << std::endl;
 	vs = { "a", "aa", "sdf", "fa", "ahg", "hju", "ut" };
-	std::cout << "Sum length : " << super_string(vs).size() << std::endl;
-	std::cout << "Superstring : " << super_string(vs) << std::endl;
+	std::cout << "Sum length : " << superString(vs).size() << std::endl;
+	std::cout << "Superstring : " << superString(vs) << std::endl;
 return 0;
 }
