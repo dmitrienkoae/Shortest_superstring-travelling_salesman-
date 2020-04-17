e8d82c492cc6ac5aa493df59b04f9910b648154e
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-17 23:00:20 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,12 @@
 #pragma once
 #include "Superstring.cpp"
 
-#include <time.h> 
-
-
-
 int main() {
-	std::vector<std::string> vs ;
-	std::string s;
-	std::string line;
-	std::ifstream in("D:\\tests2.txt"); // îêðûâàåì ôàéë äëÿ ÷òåíèÿ
-	if (in.is_open())
-	{
-		while (getline(in, line))
-		{
-			vs.push_back(line);
-		}
-	}
-	in.close();
-
-	//clock_t start = clock();
-	SumGraph g(vs);
-	std::string super_str = superString(vs);
-	//clock_t end = clock();
-
-	std::cout << super_str << std::endl;
-
-	//double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-	//std::cout << "Time : " << seconds << std::endl;
-
-	return 0;
+	std::vector<std::string> vs = {"eab", "fgh", "ghf", "hed", "abc", "cde"};
+	std::cout << "Sum length : "<< super_string(vs).size() << std::endl;
+	std::cout << "Superstring : " <<super_string(vs) << std::endl;
+	vs = { "a", "aa", "sdf", "fa", "ahg", "hju", "ut" };
+	std::cout << "Sum length : " << super_string(vs).size() << std::endl;
+	std::cout << "Superstring : " << super_string(vs) << std::endl;
+return 0;
 }
