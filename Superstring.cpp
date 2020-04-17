c38db92beeab203e1419e9d72b0163760650b79b
dmitrienkoae <54313684+dmitrienkoae@users.noreply.github.com>
2020-04-17 23:06:32 +0300

    Update Superstring.cpp

--- Superstring.cpp
+++ Superstring.cpp
@@ -1,5 +1,5 @@
 #include "FindPath.cpp"
-
+/*
 inline int SuperstringLength(const Vstr&  vs, const VVint& og, const Vint& path) {
 	if (vs.size() == 1) return vs[0].length();
 	int sup_str_len = og[path[0]][path[1]];
@@ -7,6 +7,7 @@ inline int SuperstringLength(const Vstr&  vs, const VVint& og, const Vint& path)
 		sup_str_len += og[path[i]][path[i + 1]] - vs[path[i]].length();
 	return sup_str_len;
 }
+*/
 
 //Answer
 inline std::string superString(Vstr& vs) {
@@ -20,26 +21,8 @@ inline std::string superString(Vstr& vs) {
 		int delta = matrix_overlap[path[i - 1]][path[i]];
 		super_str += vs[path[i]].substr(delta);
 	}
-	int sum_cost = SuperstringLength(vs, og, path);
-	std::cout << "Sum Cost: " << sum_cost << "\n";
+	//int sum_cost = SuperstringLength(vs, og, path);
+	std::cout << "Sum Cost: " << super_str.size() << "\n";
 	return super_str;
-
-}
-
-//For tests
-inline int superStringCost(Vstr& vs) {
-	VVint matrix_overlap = overlaps(vs);
-	SumGraph g(vs);
-	VVint og = g.m;
-	Vint path = findMinPath(g, og, matrix_overlap);
-	std::string super_str = vs[path[0]];
-	for (size_t i = 1; i < path.size(); i++) {
-		//finds prefix and cuts it
-		int delta = matrix_overlap[path[i - 1]][path[i]];
-		super_str += vs[path[i]].substr(delta);
-	}
-	int sum_cost = SuperstringLength(vs, og, path);
-	std::cout << "Sum Cost: " << sum_cost << "\n";
-	return sum_cost;
 
 }
